1822d896f3c9c2f0f5fb7ee045b4fd86da55faba
Amit Kumar <amit_kumar96@outlook.com>
2018-12-27 19:09:28 +0530

    "LeapYear" program getting committed

---
+++ FunctionalPrograms/LeapYear.py
@@ -0,0 +1,18 @@
+"""LeapYear" program tells us that the given year is a leap year
+or not a leap year.
+
+@author Amit Kumar
+@version 1.0
+@since 27/12/2018
+"""
+# importing important packages
+import utility.Utility
+
+year = input("\nPlease enter the year: ")
+# calling a function to check whether the user input year is 4 digit or not
+year = utility.Utility.get_four_digit_year(year)
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:  # checking whether the year is leap year or not
+    print("\n%s is a leap year!" % year)
+else:
+    print("\n%s is not a leap year!" % year)
