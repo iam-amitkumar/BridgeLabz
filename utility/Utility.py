07734dc59102dbfd7c221881fafc0ffbd46094b6
Amit Kumar <amit_kumar96@outlook.com>
2018-12-27 17:57:51 +0530

    "get_integer" function from utility package getting committed

--- utility/Utility.py
+++ utility/Utility.py
@@ -2,3 +2,9 @@
 def get_string():
     string = input("Please enter a word/sentence: ")
     return string
+
+
+# get_integer method takes a integer value from the user and return it
+def get_integer():
+    number = int(input("Please enter a number: "))
+    return number
