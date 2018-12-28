9b59ac0b4f66b9325d7b9248b622d6e82d8e7931
Amit Kumar <amit_kumar96@outlook.com>
2018-12-28 14:41:16 +0530

    committing "get_four_digit_year" of Utility

--- utility/Utility.py
+++ utility/Utility.py
@@ -16,11 +16,20 @@ def get_string():
 
 # get_integer function get the integer value from the user and return it
 def get_integer():
-    user_integer = input("Please enter integer value: ")
+    user_integer = int(input("Please enter integer value: "))
     return user_integer
 
 
 # get_float function get the float value from the user and return it
 def get_float():
-    user_float = input("Please enter float value: ")
+    user_float = float(input("Please enter float value: "))
     return user_float
+
+
+# this function checks whether the user-input year is four digit or not, if not throws warning with @param year
+def get_four_digit_year(year):
+    if len(year) == 4:
+        return int(year)
+    else:
+        print("\nPlease enter only four digit year.")
+        exit()
