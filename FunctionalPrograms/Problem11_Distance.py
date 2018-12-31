1c8c50eb22363e418a124e722948e7909748c9cb
Amit Kumar <amit_kumar96@outlook.com>
2018-12-31 15:18:51 +0530

    Committing "Distance" which calculates distance of a co-ordinate from origin

---
+++ FunctionalPrograms/Problem11_Distance.py
@@ -0,0 +1,18 @@
+""" that takes two integer command­line arguments x and y
+and prints the Euclidean distance from the point (x, y) to
+the origin (0, 0) where point x and y is provided through
+command-line
+
+@author Amit Kumar
+@version 1.0
+@since 31/12/2018
+"""
+# importing important modules
+import sys
+import math
+
+num1 = int(sys.argv[1])  # taking value of x from the command-line storing it into a variable
+num2 = int(sys.argv[2])  # taking value of y from the command-line storing it into a variable
+
+res = math.sqrt((num1**2)+(num2**2))  # finding the distance of point(x,y) from origin using Euclidean distance formula
+print("\nDistance of point (", num1, ",", num2,  ") from origin: ", res, "\n")
