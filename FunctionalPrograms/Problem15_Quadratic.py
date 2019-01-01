3d5bb35db4b3527abecdff021daf4d47ae800527
Amit Kumar <amit_kumar96@outlook.com>
2019-01-02 01:25:27 +0530

    Committing "Quadratic" without commenting

---
+++ FunctionalPrograms/Problem15_Quadratic.py
@@ -0,0 +1,16 @@
+"""
+
+@author Amit Kumar
+@version 1.0
+@since 01/01/2019
+"""
+# import important modules
+import utility.Utility
+print("Please enter the value of a(a should not be 0): ")
+a = utility.Utility.get_float()
+print("Please enter the value of b: ")
+b = utility.Utility.get_float()
+print("Please enter the value of c: ")
+c = utility.Utility.get_float()
+z = utility.Utility.quad_equation(a, b, c)
+print(z)
