5f5574754fc4436b532fa48518c3013c5670a9ff
Amit Kumar <amit_kumar96@outlook.com>
2019-01-02 11:26:17 +0530

    Committing "is_prime" function of Util

--- util/Util.py
+++ util/Util.py
@@ -9,6 +9,7 @@ programs by importing this module.
 
 
 # this function determine whether the given strings are anagram or not, it gives boolean result depending on result
+# contains @param string1 and @param string2 and @returns boolean value
 def is_anagram(s1, s2):
     if len(s1) == len(s2):  # checking the length of both strings, if equals then returns True
         return True
@@ -20,3 +21,20 @@ def is_anagram(s1, s2):
             return True
         else:
             return False
+
+
+# this function returns True if the number is a Prime Number having @param num and @return boolean value
+def is_prime(num):
+    count = 0
+    for i in range(2, num/2):
+        if num % i == 0:
+            count += 1
+    if count == 0:
+        return True
+
+
+
+
+
+
+
