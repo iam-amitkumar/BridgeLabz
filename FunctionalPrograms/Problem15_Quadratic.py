b4722ae8096d51db63734228854207cfa850a558
Amit Kumar <amit_kumar96@outlook.com>
2019-01-02 10:24:56 +0530

    Committing "QuadraticEquation"

--- FunctionalPrograms/Problem15_Quadratic.py
+++ FunctionalPrograms/Problem15_Quadratic.py
@@ -1,4 +1,7 @@
-"""
+""" Quadratic equation is used to find the roots of the equation a*x*x + b*x + c.
+Since the equation is x*x, hence there are 2 roots. The 2 roots of the equation
+can be found using a formula
+
 @author Amit Kumar
 @version 1.0
 @since 01/01/2019
