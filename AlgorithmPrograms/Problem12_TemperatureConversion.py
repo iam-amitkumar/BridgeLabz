0f96465c56758b89a0609c411422a32edcd509a4
Amit Kumar <amit_kumar96@outlook.com>
2019-01-03 10:22:23 +0530

    Committing "Temperature_Conversion" program

--- AlgorithmPrograms/Problem12_TemperatureConversion.py
+++ AlgorithmPrograms/Problem12_TemperatureConversion.py
@@ -1,7 +1,19 @@
+"""TemperatureConversion program converts user-input temperature
+from Celcius to Fahrenheit and vice-versa after asking from the user.
+
+@author Amit Kumar
+@version 1.0
+@since 03/01/2019
+"""
+# importing important modules
 import utility.Utility
 import util.Util
 
+# asking from the user whether he/she want to convert from Celcius to Fahrenheit aor vice-versa
 print("\nPlease enter your option: \n1. Press 1 to convert Celsius to Fahrenheit \n2. Press 2 to convert "
       "Fahrenheit to Celsius \n")
+
 t = utility.Utility.get_float()
-print("Temperature: ", util.Util.temperature_conversion(t))
+
+print("Temperature: ", util.Util.temperature_conversion(t))  # printing temperature after passing the user-input
+# to the function
