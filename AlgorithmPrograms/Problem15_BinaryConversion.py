103813ecbd6019cb635d7ee6fcbda8ecf50867b4
Amit Kumar <amit_kumar96@outlook.com>
2019-01-03 10:39:56 +0530

    Committing "BinaryConversion" program after commenting

--- AlgorithmPrograms/Problem15_BinaryConversion.py
+++ AlgorithmPrograms/Problem15_BinaryConversion.py
@@ -1,6 +1,15 @@
+"""This program is program that outputs the binary (base 2) representation of
+the decimal number typed as the input. It is based on decomposing the number into
+a sum of powers of 2.
+
+@author Amit Kumar
+@version 1.0
+@since 03/01/2019
+"""
+# importing important modules
 import utility.Utility
 import util.Util
 
-print()
-deci = utility.Utility.get_integer()
-print("Binary representation of ", deci, ": ", util.Util.to_binary(deci))
+print()  # putting one white-space line before printing anything on the console
+deci = utility.Utility.get_integer()  # calling the function
+print("Binary representation of ", deci, ": ", util.Util.to_binary(deci))  # printing the output
