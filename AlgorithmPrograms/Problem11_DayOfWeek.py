5656b02add7a9079aa574c085073db78204b2f5d
Amit Kumar <amit_kumar96@outlook.com>
2019-01-03 10:17:16 +0530

    Committing "DayOfWeek" program

--- AlgorithmPrograms/Problem11_DayOfWeek.py
+++ AlgorithmPrograms/Problem11_DayOfWeek.py
@@ -1,9 +1,16 @@
-"""
+"""DayOfWeek program takes Day, Month, Year in number format and
+returns a number 0-6 where 0 is for Sunday, 1 for AMonday and so on
+after processing the the given input using given formulae.
 
+@author Amit Kumar
+@version 1.0
+@since 03/01/2019
 """
+# importing important modules
 import sys
 
 
+# function to return number from 0 to 6 from Sunday to Saturday respectively using some formulae
 def day_of_week(d, m, y):
     y0 = y - (14 - m) // 12
     x = y0 + y0 // 4 - y0 // 100 + y0 // 400
@@ -12,6 +19,7 @@ def day_of_week(d, m, y):
     return d0
 
 
+# Assigning days of the week for the returned number b y above function
 days = {
     0: "Sunday",
     1: "Monday",
@@ -23,12 +31,13 @@ days = {
     }
 
 
+# storing values to the variables for further calculation taken through  command-line argument
 day = int(sys.argv[1])
 month = int(sys.argv[2])
 year = int(sys.argv[3])
 
 day_conversion = day_of_week(day, month, year)
-print(days.get(day_conversion, -1))
+print(days.get(day_conversion, -1))  # printing day of the week after passing values to the function
 
 
 
