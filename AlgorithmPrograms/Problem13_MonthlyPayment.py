02c6b0748e4ffac6628e2fd144d282c1cbb865ab
Amit Kumar <amit_kumar96@outlook.com>
2019-01-03 10:29:16 +0530

    Committing "MonthlyPayment" program

--- AlgorithmPrograms/Problem13_MonthlyPayment.py
+++ AlgorithmPrograms/Problem13_MonthlyPayment.py
@@ -1,6 +1,17 @@
+"""This program calculate monthlyPayment that reads in three
+command­line arguments P, Y, and R and calculates the monthly payments you
+would have to make over Y years to pay off a P principal loan amount at R per cent
+interest compounded monthly.
+
+@author Amit Kumar
+@version 1.0
+@since 03/01/2019
+"""
+# importing important modules
 import sys
 
 
+# function which calculates the monthly payment contains @param principal loan, @param rate and @param year
 def monthly_payment(principal_loan, rate, year):
     n = 12 * year
     r = rate / (12*100)
@@ -8,7 +19,9 @@ def monthly_payment(principal_loan, rate, year):
     print("Monthly Payment is: ", float(payment))
 
 
+# taking values principal, rate and year as through command-line argument and storing them into the variables
 principal = float(sys.argv[1])
 rate1 = float(sys.argv[2])
 year1 = float(sys.argv[3])
+# calling the function to calculate the monthly payment
 monthly_payment(principal, rate1, year1)
