18296ed6ee302fb84c8a58c65195c07d25f54c33
Amit Kumar <amit_kumar96@outlook.com>
2019-01-03 10:35:45 +0530

    Committing "SquareNewton" program after commenting

--- AlgorithmPrograms/Problem14_SquareNewton.py
+++ AlgorithmPrograms/Problem14_SquareNewton.py
@@ -1,13 +1,22 @@
+"""Program to compute the square root of a non-negative number c
+given in the input using Newton's method.
+
+@author Amit Kumar
+@version 1.0
+@since 03/01/2019
+"""
 
 
+# function to calculate the square root of a user-input number contains @param number, @param t and @param epsilon
+# which @return value t which is square root of the given number.
 def sqrt(no, t, epsilon):
     while abs(t-no/t) > epsilon * t:
         t = (no / t + t) / 2.0
     return t
 
 
-epsilon1 = 1e-15
-number = int(input("\nEnter the Number: "))
-t1 = number
-res = sqrt(number, t1, epsilon1)
-print("\nSquare root of ", number, ": ", res)
+epsilon1 = 1e-15  # declaring the constant value of epsilon
+c = int(input("\nEnter the Number: "))
+t1 = c  # assigning the value of user-input value c to t1 for further calculation
+res = sqrt(c, t1, epsilon1)  # calling the function
+print("\nSquare root of ", c, ": ", res)
