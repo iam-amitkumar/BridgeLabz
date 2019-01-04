0d5877ce60ee7f03ab5f96e078d00701d2e9ef70
Amit Kumar <amit_kumar96@outlook.com>
2019-01-04 18:02:08 +0530

    Committing "GuessTheNumber" program

---
+++ AlgorithmPrograms/Problem5_GuessTheNumber.py
@@ -0,0 +1,10 @@
+"""this program takes a command­line argument N, asks you
+to think of a number between 0 and N­1, where N = 2^n, and
+always guesses the answer with n questions uses Binary Search
+to find the number.
+
+@author Amit Kumar
+@version 1.0
+@since 04/01/2019
+"""
+# importing important modules
