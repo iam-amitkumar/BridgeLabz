5c3cb222cde7af605611402ab4b2467506d014b3
Amit Kumar <amit_kumar96@outlook.com>
2019-01-04 17:36:16 +0530

    Committing "BubbleSort" program

---
+++ AlgorithmPrograms/Problem8_BubbleSort.py
@@ -0,0 +1,15 @@
+"""This program Reads in integers prints them in sorted order using Bubble Sort.
+
+@author Amit Kumar
+@version 1.0
+@since 04/01/2019
+"""
+# importing important modules
+import utility.Utility
+
+input_string = input("Enter the numbers: ")
+int_list = input_string.split()  # converting user-input string to string list
+int_list = [int(i) for i in int_list]  # converting above string list to integer list
+print("\nYour numbers: ", int_list)  # printing unsorted integer list
+utility.Utility.bubble_sort(int_list)  # sorting the integer list using user-defined bubble_sort function
+print("Sorted number: ", int_list)  # printing sorted integer list
