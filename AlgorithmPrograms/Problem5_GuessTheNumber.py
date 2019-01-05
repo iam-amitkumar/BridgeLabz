c20bc2e707e91446251dcc25b18fdfae86a8fd1d
Amit Kumar <amit_kumar96@outlook.com>
2019-01-05 09:43:53 +0530

    recommitting "GuessTheNumber" Program

--- AlgorithmPrograms/Problem5_GuessTheNumber.py
+++ AlgorithmPrograms/Problem5_GuessTheNumber.py
@@ -26,12 +26,12 @@ def question(low, high):
         return 0
 
 
-noOfTimes = int(input("How much time you want to ask the question:"))
+noOfTimes = int(input("\nHow much time you want to ask the question:"))
 low1 = 0  # assigning low value for further search of the number on the principal of binary search
 high1 = int(math.pow(2, noOfTimes))  # assigning high as user-input value on the power of 2
 print()
-print("Think a number between(", low1, ")to(", high1, ")in range")  # asking the user that their value lies in which
+print("Think a number between ", low1, " to ", high1-1, " in range")  # asking the user that their value lies in which
 # part of the range, whether in first part or second part and continue this process until we got the number
 print()
 res = question(low1, high1)  # calling the function
-print("\nYou guessed :", res)  # printing the result
+print("\nSurprise!!! Surprise!!! We got it, You guessed :", res)  # printing the result
