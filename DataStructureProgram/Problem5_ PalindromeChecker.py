1c0b0f9d6f6e4767e07decca92895fe2e2a9fb76
Amit Kumar <amit_kumar96@outlook.com>
2019-01-09 14:42:23 +0530

    Committing "PalindromeChecker" Program

--- DataStructureProgram/Problem5_ PalindromeChecker.py
+++ DataStructureProgram/Problem5_ PalindromeChecker.py
@@ -12,41 +12,69 @@ character to the rear of the deque.
 from DataStructureProgram.Deque import *
 
 
-# this method create object of Deque class, take a string from user and return True or False basis on the result
-def palindrome_checker():
-    d1 = Deque()  # creating object of the Deque class
-    global string  # making variable "string" global to use it in try block
-    try:
-        string = input("Enter String to Check for Palindrome: ")
-    except Exception as e:  # handling exception for user-input
-        print(e)
-    lower_string = string.lower()  # converting the user-input string to lower case
-    string = lower_string  # assigning converted user-input sentence to the variable "string"
+def is_palindrome(word):
+    queue = Deque()  # creating object of Deque class
 
-    remove_space = ''
-    for i in range(0, len(string)):  # removing blank space from the string
-        if string[i] == ' ':
-            continue
+    for ch in word:
+        queue.insert_rear(ch)  # adds all the characters from back
 
-        remove_space += string[i]
+    is_equal = True
 
-    string = remove_space
+    while queue.size() > 1 and is_equal:
+        first = queue.remove_front()  # removes the characters from the front of the queue
+        last = queue.remove_rear()  # removes the characters from the rear of the queue
+        if first != last:
+            is_equal = False
 
-    for i in string:
-        d1.insert_rear(i)  # queuing the letters of the string to the queue from rear side
-    reverse_string = ''
-    for i in range(0, d1.size()):
-        reverse_string += str(d1.delete_rear())  # trimming the letters of the deque and adding them to the string
-        # "reverse_string", i.e., reversing the string
-
-# comparing the the reversed string with the original string
-    if string == reverse_string:
-        return True  # returning True if both strings matched
-    else:
-        return False  # returning False if both strings not matched
+    return is_equal
 
 
-if palindrome_checker():
-    print("\nYour string is Palindrome")
+word_ = input("\nEnter the word ")
+
+if is_palindrome(word_):
+    print("\n'", word_, "' is Palindrome")
 else:
-    print("\nYour string is not palindrome")
+    print("\n'", word_, "' is not Palindrome")
+
+
+# from DataStructureProgram.Deque import *
+#
+#
+# # this method create object of Deque class, take a string from user and return True or False basis on the result
+# def palindrome_checker():
+#     d1 = Deque()  # creating object of the Deque class
+#     global string  # making variable "string" global to use it in try block
+#     try:
+#         string = input("Enter String to Check for Palindrome: ")
+#     except Exception as e:  # handling exception for user-input
+#         print(e)
+#     lower_string = string.lower()  # converting the user-input string to lower case
+#     string = lower_string  # assigning converted user-input sentence to the variable "string"
+#
+#     remove_space = ''
+#     for i in range(0, len(string)):  # removing blank space from the string
+#         if string[i] == ' ':
+#             continue
+#
+#         remove_space += string[i]
+#
+#     string = remove_space
+#
+#     for i in string:
+#         d1.insert_rear(i)  # queuing the letters of the string to the queue from rear side
+#     reverse_string = ''
+#     for i in range(0, d1.size()):
+#         reverse_string += str(d1.delete_rear())  # trimming the letters of the deque and adding them to the string
+#         # "reverse_string", i.e., reversing the string
+#
+# # comparing the the reversed string with the original string
+#     if string == reverse_string:
+#         return True  # returning True if both strings matched
+#     else:
+#         return False  # returning False if both strings not matched
+#
+#
+# if palindrome_checker():
+#     print("\nYour string is Palindrome")
+# else:
+#     print("\nYour string is not palindrome")
