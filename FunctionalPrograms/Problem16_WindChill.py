f2bf44bf6e4057bfe81474a0f42fc5797d4d0dbc
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:55:05 +0530

    Committing "WindChill" program

--- FunctionalPrograms/Problem16_WindChill.py
+++ FunctionalPrograms/Problem16_WindChill.py
@@ -12,7 +12,7 @@ import sys
 
 def wind_chill_prob(t, v):
     w = 0.0
-    if t <= 50 and (t <= 120 and t >= 3):  # applying condition when value of 't' is valid
+    if (t <= 50) and (t <= 120) and (t >= 3):  # applying condition when value of 't' is valid
         w = 35.74+(0.6215*t)+(((0.4275*t)-35.75) * (v ** 0.16))
     else:
         print("Value of 't' should not exceed more than 50\nValue of 'v' should not exceed more than 120 or less "
@@ -20,7 +20,13 @@ def wind_chill_prob(t, v):
     return w
 
 
-t1 = int(sys.argv[1])  # getting value of t from command-line argument from user
-v1 = int(sys.argv[2])  # getting value of v from command-line argument from user
+global t1, v1
+
+try:
+    t1 = int(sys.argv[1])  # getting value of t from command-line argument from user
+    v1 = int(sys.argv[2])  # getting value of v from command-line argument from user
+except Exception as e:
+    print(e)
+
 res = wind_chill_prob(t1, v1)
 print("Effective temperature (the wind chill): ", res)
