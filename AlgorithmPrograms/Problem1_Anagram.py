9efd16a9c3501b54162346274ebe11b2b61c1225
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:39:03 +0530

    Committing "Anagram"

--- AlgorithmPrograms/Problem1_Anagram.py
+++ AlgorithmPrograms/Problem1_Anagram.py
@@ -9,8 +9,14 @@ strings are anagram or not.
 import utility.Utility
 import util.Util
 
-s1 = utility.Utility.get_string()
-s2 = utility.Utility.get_string()
+global s1, s2
+
+try:
+    s1 = utility.Utility.get_string()
+    s2 = utility.Utility.get_string()
+except Exception as e:
+    print(e)
+
 res = util.Util.is_anagram(s1, s2)
 if res is True:  # checking whether the function returns true or not, of true then both the strings are anagram
     print("\n", s1, "and ", s2, " are anagram string")
