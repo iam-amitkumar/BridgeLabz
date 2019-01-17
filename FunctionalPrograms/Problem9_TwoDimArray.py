ad92425c8946b5dd012007131822be8eaf48c634
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:53:08 +0530

    Committing "TwoDimArray" to print user-input matrix of integer, float or boolean

--- FunctionalPrograms/Problem9_TwoDimArray.py
+++ FunctionalPrograms/Problem9_TwoDimArray.py
@@ -7,22 +7,41 @@ of integer, float and boolean type.
 """
 import utility.Utility
 
-int_rows = int(input('enter number of rows for integer matrix :'))
-int_columns = int(input('enter number of columns for integer matrix :'))
-float_rows = int(input('\nenter number of rows for float matrix: '))
-float_columns = int(input('enter number of columns for float integer matrix: '))
-boolean_rows = int(input('\nenter number of rows for boolean matrix: '))
-boolean_columns = int(input('enter number of columns for boolean matrix: '))
+global int_rows, int_columns, float_rows, float_columns, boolean_rows, boolean_columns, int_mat, float_mat, boolean_mat
+
+try:
+    int_rows = int(input('enter number of rows for integer matrix :'))
+    int_columns = int(input('enter number of columns for integer matrix :'))
+    float_rows = int(input('\nenter number of rows for float matrix: '))
+    float_columns = int(input('enter number of columns for float integer matrix: '))
+    boolean_rows = int(input('\nenter number of rows for boolean matrix: '))
+    boolean_columns = int(input('enter number of columns for boolean matrix: '))
+
+except Exception as e:
+    print(e)
+
 print()
 
 # user defined integer matrix
-int_mat = utility.Utility.int_two_dimensional_array(int_rows, int_columns)
+try:
+    int_mat = utility.Utility.int_two_dimensional_array(int_rows, int_columns)
+except Exception as e:
+    print(e)
+
 print()
 # user defined float matrix
-float_mat = utility.Utility.float_two_dimensional_array(float_rows, float_columns)
+try:
+    float_mat = utility.Utility.float_two_dimensional_array(float_rows, float_columns)
+except Exception as e:
+    print(e)
+
 print()
 # user defined boolean matrix
-boolean_mat = utility.Utility.boolean_two_dimensional_array(boolean_rows, boolean_columns)
+try:
+    boolean_mat = utility.Utility.boolean_two_dimensional_array(boolean_rows, boolean_columns)
+except Exception as e:
+    print(e)
+
 print()
 
 # display integer matrix
