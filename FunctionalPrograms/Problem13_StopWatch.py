41dfb128063c8c295f02c07a921ca2c95321bd88
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:54:21 +0530

    Committing "Stopwatch" program

--- FunctionalPrograms/Problem13_StopWatch.py
+++ FunctionalPrograms/Problem13_StopWatch.py
@@ -10,6 +10,7 @@ the time to find the difference between them)
 import utility.Utility
 import datetime
 
+global option
 start = 0
 stop = 0
 loop_it = True  # to get the option to stop the stop watch after starting it
@@ -19,7 +20,11 @@ while loop_it:
     print("\nPress a button:\n"
           "1. Press 1 to start the stopwatch\n"
           "2. Press 2 to stop the stopwatch")
-    option = utility.Utility.get_integer()  # getting the input from the user to start or stop the stopwatch
+    try:
+        option = utility.Utility.get_integer()  # getting the input from the user to start or stop the stopwatch
+    except Exception as e:
+        print(e)
+
     if option == 1:
         start = datetime.datetime.now()  # when the stopwatch get started then storing current time in start variable
         check_start = True  # changing the status of "check_start" to True to assure that the stopwatch is started
@@ -31,4 +36,3 @@ while loop_it:
     else:
         print("\nInvalid Option, please provide a valid input.")
         break
-
