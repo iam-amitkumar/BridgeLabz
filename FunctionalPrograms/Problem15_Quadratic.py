59805cbcf071453c93fd7863a5cf0bf9560d5020
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:54:52 +0530

    Committing "QuadraticEquation"

--- FunctionalPrograms/Problem15_Quadratic.py
+++ FunctionalPrograms/Problem15_Quadratic.py
@@ -6,12 +6,17 @@ can be found using a formula
 @version 1.0
 @since 01/01/2019
 """
+
 # import important modules
 import utility.Utility
-print("Please enter the value of a(a should not be 0): ")
-a = utility.Utility.get_float()
-print("Please enter the value of b: ")
-b = utility.Utility.get_float()
-print("Please enter the value of c: ")
-c = utility.Utility.get_float()
-print(utility.Utility.quad_equation(a, b, c))
+
+try:
+    print("Please enter the value of a(a should not be 0): ")
+    a = utility.Utility.get_float()
+    print("Please enter the value of b: ")
+    b = utility.Utility.get_float()
+    print("Please enter the value of c: ")
+    c = utility.Utility.get_float()
+    print(utility.Utility.quad_equation(a, b, c))
+except Exception as e:
+    print(e)
