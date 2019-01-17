8ec5eb982e65846909a8e1fa92fb74b89aeaf8ca
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:53:46 +0530

    Committing "Distance" which calculates distance of a co-ordinate from origin

--- FunctionalPrograms/Problem11_Distance.py
+++ FunctionalPrograms/Problem11_Distance.py
@@ -11,8 +11,13 @@ command-line
 import sys
 import math
 
-num1 = int(sys.argv[1])  # taking value of x from the command-line storing it into a variable
-num2 = int(sys.argv[2])  # taking value of y from the command-line storing it into a variable
+global num1, num2
+
+try:
+    num1 = int(sys.argv[1])  # taking value of x from the command-line storing it into a variable
+    num2 = int(sys.argv[2])  # taking value of y from the command-line storing it into a variable
+except Exception as e:
+    print(e)
 
 res = math.sqrt((num1**2)+(num2**2))  # finding the distance of point(x,y) from origin using Euclidean distance formula
 print("\nDistance of point (", num1, ",", num2,  ") from origin: ", res, "\n")
