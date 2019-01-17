c47f44481cd9da4487a295317c3950e9f8cd32fe
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:42:07 +0530

    Committing "BinarySearch" program

--- AlgorithmPrograms/Problem6_BinarySearch.py
+++ AlgorithmPrograms/Problem6_BinarySearch.py
@@ -10,12 +10,26 @@ search the list. The program reports if the search word is found in the list or 
 import utility.Utility
 import string
 
-open_file = open("binary.txt")  # opening text file named "binary.txt"
-sen_split = list(open_file.read().split())  # adding each word of every sentence to the list "sen_split" as an item
-word = [word.strip(string.punctuation) for word in sen_split]  # trimming all commas/punctuation present in the sentence
-utility.Utility.insertion_sort(word)  # sorting the comma/punctuation trimmed list using user-defined function
+global user_input, word, open_file
 
-user_input = input("\nEnter your word to search in the file: ")
+try:
+    open_file = open("binary.txt")  # opening text file named "binary.txt"
+    sen_split = list(open_file.read().split())  # adding each word of every sentence to the list "sen_split" as an item
+    word = [word.strip(string.punctuation) for word in sen_split]  # trimming all commas/punctuation present in the
+    # sentence
+    utility.Utility.insertion_sort(word)  # sorting the comma/punctuation trimmed list using user-defined function
+except FileNotFoundError as e:
+    print("\nYour file is not available on the drive.")
+finally:
+    try:
+        open_file.close()
+    except Exception as e:
+        print("\n", e)
+
+try:
+    user_input = input("\nEnter your word to search in the file: ")
+except Exception as e:
+    print(e)
 
 res = utility.Utility.binary_search(word, user_input)  # searching the user-input string in the sorted list "word"
 # using user-defined function "binary_search" which @return boolean value having @param list and @string to search
@@ -24,4 +38,3 @@ if res:
     print("\nYes, '%s' is present in the file." % user_input)
 else:
     print("\nNo, %s is not present in the file." % user_input)
-
