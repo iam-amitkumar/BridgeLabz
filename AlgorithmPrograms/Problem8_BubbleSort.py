b05bc1898f9c9b3965505d8b40389888c91f2680
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:42:33 +0530

    Committing "BubbleSort" program

--- AlgorithmPrograms/Problem8_BubbleSort.py
+++ AlgorithmPrograms/Problem8_BubbleSort.py
@@ -7,7 +7,13 @@
 # importing important modules
 import utility.Utility
 
-input_string = input("Enter the numbers: ")
+global input_string
+
+try:
+    input_string = input("Enter the numbers: ")
+except Exception as e:
+    print(e)
+
 int_list = input_string.split()  # converting user-input string to string list
 int_list = [int(i) for i in int_list]  # converting above string list to integer list
 print("\nYour numbers: ", int_list)  # printing unsorted integer list
