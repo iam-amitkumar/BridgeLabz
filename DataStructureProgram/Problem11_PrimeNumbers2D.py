2e89b2ec9bee2a3301d146cf3c6af9d9c56f155d
Amit Kumar <amit_kumar96@outlook.com>
2019-01-17 16:29:08 +0530

    Committing "PrimeNumber2D" Program

--- DataStructureProgram/Problem11_PrimeNumbers2D.py
+++ DataStructureProgram/Problem11_PrimeNumbers2D.py
@@ -25,7 +25,7 @@ print()
 prime = [["0-100    :"], ["100-200  :"], ["200-300  :"], ["300-400  :"], ["400-500  :"], ["500-600  :"], ["600-700  :"],
          ["700-800  :"], ["800-900  :"], ["900-1000 :"]]
 
-# appending all the prime numbers in the list
+# adding all the prime numbers from 2 to 1000 in the list in string form
 for i in range(2, 1000+1):
     if prime_or_not(i):
         prime[int(i/100)].append(str(i))
@@ -33,5 +33,5 @@ for i in range(2, 1000+1):
 # organising all the prime numbers in their respective row according to the value
 for i in range(len(prime)):
     for j in range(len(prime[i])):
-        print(prime[i][j], end=" ")
-    print()
+        print(prime[i][j], end=" ")  # printing the values in their respective row  separated by whitespace
+    print()  # changing line after each row
